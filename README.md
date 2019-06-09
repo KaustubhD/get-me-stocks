9b696bbacee79d47686eea64c42702689c1f8f2f
Glitch (get-me-stocks) <none>
2019-06-09 21:10:26 +0000

    💑🏓 Single req objective logic

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
-**FreeCodeCamp**- Information Security and Quality Assurance
+Stock price tracker
 ------
+On the front-end,
+- edit `public/client.js`, `public/style.css` and `views/index.html`
+- drag in `assets`, like images or music, to add them to your project
 
-Project Stock Price Checker
-
-1) SET NODE_ENV to `test` without quotes and set DB to your mongo connection string
-2) Complete the project in `routes/api.js` or by creating a handler/controller
-3) You will add any security features to `server.js`
-4) You will create all of the functional tests in `tests/2_functional-tests.js`
+On the back-end,
+- your app starts at `server.js`
+- add frameworks and packages in `package.json`
+- safely store app secrets in `.env` (nobody can see this but you and people you invite)
 
 
+Example requests
+-------------------
+* /api/stock-prices?stock=goog
+* /api/stock-prices?stock=goog&like=true
+* /api/stock-prices?stock=goog&stock=msft
+* /api/stock-prices?stock=goog&stock=msft&like=true
+
+
+Example responses
+-------------------
+* `{"stockData":{"stock":"GOOG","price":"786.90","likes":1}}`
+* `{"stockData":[{"stock":"MSFT","price":"62.30","rel_likes":-1},{"stock":"GOOG","price":"786.90","rel_likes":1}]}`
+
+
+Objectives
+-------------------
+- [ ] Set the content security policies to only allow loading of scripts and css from your server.
+- [x] I can GET /api/stock-prices with form data containing a Nasdaq stock ticker and recieve back an object stockData.
+- [x] In stockData, I can see the stock(string, the ticker), price(decimal in string format), and likes(int).
+- [x] I can also pass along field like as true(boolean) to have my like added to the stock(s). Only 1 like per ip should be accepted.
+- [ ] If I pass along 2 stocks, the return object will be an array with both stock's info but instead of likes, it will display rel_likes(the difference between the likes on both) on both.
+- [ ] A good way to receive current price is the following external API(replacing 'GOOG' with your stock): https://finance.google.com/finance/info?q=NASDAQ%3aGOOG
+- [ ] All 5 functional tests are complete and passing.
+
+Testing
+-------------------
+For testing, the ```NODE_ENV``` variable needs to be set to testin the **.env** file
+
+
+Live Project
+-------------------
+[\ ゜o゜)ノ](https://get-me-stocks.glitch.me)
--- routes/api.js
+++ routes/api.js
@@ -4,36 +4,76 @@ var expect = require('chai').expect
 var MongoClient = require('mongodb')
 const fetch = require('node-fetch')
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+
+
+function cleanUpStockObject(obj, apiData){
+  delete obj.stockData.symbol
+  delete obj.stockData._id
+  obj.stockData.stock = apiData['Global Quote']['01. symbol']
+  obj.stockData.price = apiData['Global Quote']['05. price']
+  return obj
+}
+
+
+async function getData(stock){
+  // console.log('Calling')
+  return await MongoClient.connect(CONNECTION_STRING, {useNewUrlParser: true})
+  .then(db => {
+    return db.db().collection('stocks')
+    .findOne({symbol: stock.symbol})
+    .then(doc => {
+      //check for ip
+      // console.log(doc)
+      const isValidLike = !(doc && doc.ip && doc.ip.includes(stock.ip))
+      return db.db().collection('stocks')
+      .findOneAndUpdate({symbol: stock.symbol}, {
+        $setOnInsert: {
+          symbol: stock.symbol,
+          // price: stock.price
+        },
+        $addToSet: {
+          ip: stock.like && stock.ip
+        },
+        $inc: {
+          likes: isValidLike && stock.like ? 1 : 0
+        }
+      }, {
+        upsert: true,
+        returnOriginal: false
+      })
+      .then(doc => doc.value)
+    })
+    .catch(err => console.error(err))
+  })
+  .catch(err => console.error('Error in connection', err))
+}
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      let stock = req.query.stock
+      
+      let symbol = req.query.stock
       let ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).split(',')[0]
-      let like = req.query.lik ? 1 : 0
+      let like = req.query.like ? 1 : 0
       
+      const noName = {symbol, ip, like}
       
-      if(typeof stock == 'string'){
+      if(typeof symbol == 'string'){
+        
         fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.query.stock}&apikey={process.env.KEY}`)
         .then(res => res.json())
-        .then(data => {
-          
-          console.log(data)
-        
-        
+        .then(async data => {
+          let stock = {}
+          await getData(noName).then(st => stock.stockData = st)
+          stock = cleanUpStockObject(stock, data)
+          res.json(stock)
         })
-        
-        
-      
-      
       
       }
     
-    
-    
-      else if(Array.isArray(stock)){
+      else if(Array.isArray(symbol)){
         
         
         
